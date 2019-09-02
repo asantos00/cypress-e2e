9f7cb210aa66634248d83a1445ba280b158ec0e4
Alexandre Santos <alexandre.santozz@gmail.com>
2019-09-02 22:52:04 +0100

    Unfix sporting

--- cypress/integration/basic.js
+++ cypress/integration/basic.js
@@ -9,7 +9,7 @@ describe("Basic rendering", () => {
   it('renders games from 3 major teams', () => {
     cy.get("main").contains("Benfica");
     cy.get("main").contains("Porto");
-    cy.get("main").contains("Sporting");
+    cy.get("main").contains("Braga");
   })
 
   it('renders cart', () => {
